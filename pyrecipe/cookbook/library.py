0b60c62ddeea1fd2c87e46a8927b59b132f8ec6a
Robert Hughes <robert.hughes@bt.com>
2018-12-28 11:02:48 +0000

    add docstrings and type hints

--- pyrecipe/cookbook/library.py
+++ pyrecipe/cookbook/library.py
@@ -1,9 +1,13 @@
 import abc
+from recipe import Recipe
 
 class LibraryInterface(metaclass=abc.ABCMeta):
+    """
+    A class for interacting with the PyRecipe database.
+    """
     @abc.abstractmethod
     @staticmethod
-    def find_recipe_by_name(name):
+    def find_by_name(name: str) -> Recipe:
         """
         Library.find_recipe_by_name('Vietnamese Pork')
 
@@ -14,9 +18,9 @@ class LibraryInterface(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     @staticmethod
-    def find_recipes_by_field(tags=None, ingredients=None):
+    def find_by_field(tags: list = None, ingredients: list = None) -> list:
         """
-        Library.find_recipes_by_field(tags=['mexican', 'hot'], ingredients=['pepper'])
+        Library.find_by_field(tags=['mexican', 'hot'], ingredients=['pepper'])
 
         :param tags: A list of tags.
         :param ingredients: A list of ingredients.
@@ -26,9 +30,9 @@ class LibraryInterface(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     @staticmethod
-    def delete_recipe(recipe):
+    def delete(recipe: Recipe) -> dict:
         """
-        Library.delete_recipe(recipe)
+        Library.delete(recipe)
 
         :param recipe: A recipe object.
         :returns: {'success': boolean, 'cached_until': datetime}
@@ -37,9 +41,9 @@ class LibraryInterface(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     @staticmethod
-    def add_recipe(recipe):
+    def add(recipe: Recipe) -> dict:
         """
-        Library.add_recipe(recipe)
+        Library.add(recipe)
 
         :param recipe: A recipe object.
         :return: {'success': boolean, 'recipe': recipe}
@@ -47,9 +51,12 @@ class LibraryInterface(metaclass=abc.ABCMeta):
         raise NotImplementedError
 
 class TagsInterface(metaclass=abc.ABCMeta):
+    """
+    A class for interacting with tags in the PyRecipe database.
+    """
     @abc.abstractmethod
     @staticmethod
-    def list():
+    def list() -> list:
         """
         Tags.list()
 
@@ -59,7 +66,7 @@ class TagsInterface(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     @staticmethod
-    def add(tag):
+    def add(tag) -> bool:
         """
         Tags.add(['hot', 'spicy'])
 
@@ -69,9 +76,12 @@ class TagsInterface(metaclass=abc.ABCMeta):
         raise NotImplementedError
 
 class IngredientsInterface(metaclass=abc.ABCMeta):
+    """
+    A class for interacting with ingredients in the PyRecipe database.
+    """
     @abc.abstractmethod
     @staticmethod
-    def list():
+    def list() -> list:
         """
         Ingredients.list()
 
@@ -81,9 +91,10 @@ class IngredientsInterface(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     @staticmethod
-    def add(ingredients: list):
+    def add(ingredients: list) -> bool:
         """
         Ingredients.add(['red pepper', 'green pepper'])
+
         :param ingredients:
         :return: Boolean based on success.
         """
