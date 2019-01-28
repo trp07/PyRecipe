69a9df0a5ff8ab095baaf2926e0cbb0f4edcacf6
Timoteo <phillipstr@gmail.com>
2019-01-28 21:59:25 +0300

    adds verbose output to mongo_setup.global_init()

--- pyrecipe/storage/mongo_setup.py
+++ pyrecipe/storage/mongo_setup.py
@@ -3,6 +3,8 @@
 import mongoengine
 
 
-def global_init(db_name="pyrecipe"):
+def global_init(db_name="pyrecipe", verbose=False):
     """Create/Register connection with mongodb.  DB name will be 'pyrecipe'."""
     mongoengine.register_connection(alias="core", name=db_name)
+    if verbose:
+        print("[+] MongoDB connection registered to database: {}".format(db_name))
