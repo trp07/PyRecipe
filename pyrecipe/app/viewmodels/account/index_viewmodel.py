727009b715b217f4ff253701ca299ebddc2ad666
Timoteo <phillipstr@gmail.com>
2020-07-02 22:05:46 -0400

    removes old import statements

--- pyrecipe/app/viewmodels/account/index_viewmodel.py
+++ pyrecipe/app/viewmodels/account/index_viewmodel.py
@@ -1,5 +1,5 @@
 from pyrecipe.app.viewmodels.shared import ViewModelBase
-from pyrecipe.storage.user import User
+
 
 
 class IndexViewModel(ViewModelBase):
--- pyrecipe/app/viewmodels/home/about_viewmodel.py
+++ pyrecipe/app/viewmodels/home/about_viewmodel.py
@@ -1,5 +1,4 @@
 from pyrecipe.app.viewmodels.shared import ViewModelBase
-from pyrecipe.storage.user import User
 
 
 class AboutViewModel(ViewModelBase):
