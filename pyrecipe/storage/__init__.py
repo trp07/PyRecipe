d3e6dced72cc4349c34d06bf97892826a41407c4
Timoteo <phillipstr@gmail.com>
2020-07-02 21:59:51 -0400

    removes old imports

--- pyrecipe/storage/__init__.py
+++ pyrecipe/storage/__init__.py
@@ -1,8 +1,2 @@
 from . import shared
 from . import mongo
-
-from . import mongo_setup
-from .ingredient import Ingredient
-from .image import Image
-from .recipe import Recipe
-from .user import User
