8a5c2aaab4525b18a021287a8ac037dbb47985db
Timoteo <phillipstr@gmail.com>
2020-08-02 20:30:53 -0400

    adds tests for home views

---
+++ tests/app/viewmodels/test_vmhome.py
@@ -0,0 +1,62 @@
+"""Tests for pyrecipe/app/viewmodels."""
+
+from collections import namedtuple
+
+import pytest
+
+from flask import Response
+
+from pyrecipe.usecases.account_uc import AccountUC
+from pyrecipe.app.viewmodels.home import AboutViewModel
+from pyrecipe.app.viewmodels.home import IndexViewModel
+from pyrecipe.app import app as flask_app
+
+
+USER = namedtuple("User", ["name"])
+
+
+def test_idxvm_nouser(mocker):
+    """
+    GIVEN a no logged-in user
+    WHEN a view request for /index is initiated
+    THEN assert no errors and the user is "Guest"
+    """
+    target = mocker.patch.object(AccountUC, "find_user_by_id")
+    target.return_value = None
+    with flask_app.test_request_context(path='/account/register', data=None):
+        vm = IndexViewModel()
+
+    vm.validate()
+    result = vm.to_dict()
+    assert vm.error is None
+    assert result["name"] == "Guest"
+
+
+def test_idxvm_withuser(mocker):
+    """
+    GIVEN a logged in user
+    WHEN a view request for /index is initiated
+    THEN assert no errors and the user is the logged in user
+    """
+    target = mocker.patch.object(AccountUC, "find_user_by_id")
+    target.return_value = USER("Dude")
+    with flask_app.test_request_context(path='/account/register', data=None):
+        vm = IndexViewModel()
+
+    vm.validate()
+    result = vm.to_dict()
+    assert vm.error is None
+    assert result["name"] == "Dude"
+
+
+def test_aboutvm(mocker):
+    """
+    GIVEN a running app
+    WHEN /about view is requested
+    THEN assert no errors
+    """
+    target = mocker.patch.object(AccountUC, "find_user_by_id")
+    target.return_value = None
+    with flask_app.test_request_context(path='/account/register', data=None):
+        vm = AboutViewModel()
+    assert isinstance(vm, AboutViewModel)
---
+++ tests/app/views/test_apphome.py
@@ -0,0 +1,43 @@
+"""Tests for pyrecipe/app/views."""
+
+import pytest
+
+from flask import Response
+
+from pyrecipe.usecases.account_uc import AccountUC
+from pyrecipe.usecases.recipe_uc import RecipeUC
+from pyrecipe.app import app as flask_app
+from pyrecipe.app.views import home_views
+from pyrecipe.app.viewmodels.home import IndexViewModel
+from pyrecipe.app.viewmodels.home import AboutViewModel
+
+
+def test_indexview(mocker):
+    """
+    GIVEN a running app
+    WHEN a view is requested for /index
+    THEN assert no errors
+    """
+    acct = mocker.patch.object(AccountUC, "find_user_by_id")
+    recipes = mocker.patch.object(RecipeUC, "get_all_recipes")
+    tags = mocker.patch.object(RecipeUC, "get_tags")
+    idxvm = mocker.patch.object(IndexViewModel, "__call__")
+
+    with flask_app.test_request_context(path="/index", data=None):
+        resp: Response = home_views.index()
+    assert resp.location is None
+
+
+def test_aboutview(mocker):
+    """
+    GIVEN a running app
+    WHEN a view is requested for /about
+    THEN assert no errors
+    """
+    acct = mocker.patch.object(AccountUC, "find_user_by_id")
+    aboutvm = mocker.patch.object(AboutViewModel, "__call__")
+
+    with flask_app.test_request_context(path="/index", data=None):
+        resp: Response = home_views.about()
+    assert resp.location is None
+
