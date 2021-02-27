ac519a280ab0964bea3758f9cd52b9036bb0db6e
Timoteo <phillipstr@gmail.com>
2021-02-26 20:58:52 -0500

    redo the usecase tests by Mocking out the database instead of using a fake DB instance that is less realistic.

--- tests/usecases/test_uc_account.py
+++ tests/usecases/test_uc_account.py
@@ -1,44 +1,51 @@
 """Test for the usecases/account_uc.py module."""
 
+from unittest.mock import Mock
+
 import pytest
 
 from pyrecipe.usecases.account_uc import AccountUC
 
 
-def test_recipeuc_instantiation(act_driver):
+def test_accountuc_instantiation():
     """
     GIVEN a DB driver to instantiate in the Use Case
     WHEN instantiating
     THEN assert it is created
     """
-    a = AccountUC(act_driver)
+    a = AccountUC(Mock())
     assert a._driver is not None
 
 
-def test_login_user(act_driver):
+def test_login_user():
     """
     GIVEN users in the DB
     WHEN trying to log in
     THEN assert user logs in
     """
-    a = AccountUC(act_driver)
+    a = AccountUC(Mock())
+    a._driver.user_login.return_value = "UserModel"
+
     result = a.login_user("bob@mail.com", "bobrulz")
-    assert result is not None
+    assert result == "UserModel"
 
 
-def test_register_user(act_driver):
+def test_register_user():
     """
     GIVEN a new user to register
     WHEN registering
     THEN assert user is returned
     """
-    a = AccountUC(act_driver)
+    a = AccountUC(Mock())
+    a._driver.user_create.return_value = "UserModel"
+
     user = a.register_user("arthur", "arthur@mail.com", "arthurrulz")
-    assert user.name == "arthur"
+    assert user == "UserModel"
 
 
-@pytest.mark.parametrize("_id, expected", [(123, "alice"), (456, "bob")])
-def test_find_user_by_id(act_driver, _id, expected):
-    a = AccountUC(act_driver)
-    result = a.find_user_by_id(_id)
-    assert result.name == expected
+def test_find_user_by_id():
+    a = AccountUC(Mock())
+    a._driver.user_find_by_id.return_value = "UserModel"
+
+    result = a.find_user_by_id("12345")
+    assert result == "UserModel"
--- tests/usecases/test_uc_recipe.py
+++ tests/usecases/test_uc_recipe.py
@@ -3,6 +3,7 @@
 import builtins
 import datetime
 import uuid
+from unittest.mock import Mock
 
 import pytest
 
@@ -13,49 +14,57 @@ from pyrecipe.usecases.recipe_uc import RecipeUC
 from pyrecipe.services import export
 
 
-def test_recipeuc_instantiation(rec_driver):
+def test_recipeuc_instantiation():
     """
     GIVEN a DB driver to instantiate in the Use Case
     WHEN instantiating
     THEN assert it is created
     """
-    r = RecipeUC(rec_driver)
+    r = RecipeUC(Mock())
     assert r._driver is not None
 
 
-@pytest.mark.parametrize("deleted, expected", [(True, 1), (False, 1), (None, 2)])
-def test_get_all_recipes(rec_driver, deleted, expected):
+@pytest.mark.parametrize("deleted, expected", [(False,[1,0,0]), (True,[0,1,0]), (None,[0,0,1])])
+def test_get_all_recipes(deleted, expected):
     """
     GIVEN recipes
     WHEN asking for those by deleted=X
-    THEN assert the correct # are returned
+    THEN assert the correct db functions are called
     """
-    r = RecipeUC(rec_driver)
+    r = RecipeUC(Mock())
     recipes = r.get_all_recipes(deleted=deleted)
-    assert len(recipes) == expected
+    assert r._driver.recipes_active.call_count == expected[0]
+    assert r._driver.recipes_deleted.call_count == expected[1]
+    assert r._driver.recipes_all.call_count == expected[2]
 
 
-def test_find_recipe_by_id(rec_driver):
+def test_find_recipe_by_id():
     """
     GIVEN recipes
     WHEN asking for a recipe by it's DB id
-    THEN assert it is returned
+    THEN assert then assert the DB calls are made
     """
-    r = RecipeUC(rec_driver)
+    class Recipe:
+        images = ["testimg.jpg"]
+
+    r = RecipeUC(Mock())
+    r._driver.recipe_find_by_id.return_value = Recipe()
+
     recipe = r.find_recipe_by_id("123")
-    assert recipe.name == "recipe1"
+    assert recipe is not None
     assert recipe.images == ["../../static/img/recipe_images/testimg.jpg"]
 
 
-def test_create_recipe(rec_driver, mocker):
+def test_create_recipe(mocker):
     """
     GIVEN a recipe to create
     WHEN creating it
-    THEN assert it is returned
+    THEN assert the correct sequence of events occur
     """
+    r = RecipeUC(Mock())
     proc_img_mock = mocker.patch.object(ruc, "process_image")
-    r = RecipeUC(rec_driver)
-    result = r.create_recipe(
+
+    recipe = r.create_recipe(
         name="test",
         prep_time=5,
         cook_time=10,
@@ -67,16 +76,16 @@ def test_create_recipe(rec_driver, mocker):
         images=["imagefile1.jpg", "imagefile2.jpg"],
     )
     assert proc_img_mock.call_count == 2
-    assert result.name == "test"
+    assert r._driver.recipe_create.call_count == 1
 
 
-def test_edit_recipe(rec_driver):
+def test_edit_recipe():
     """
     GIVEN a recipe to edit
     WHEN submitting new attributes
-    THEN assert it is returned with new attributes saved
+    THEN assert the correct sequence of events occur
     """
-    r = RecipeUC(rec_driver)
+    r = RecipeUC(Mock())
     result = r.edit_recipe(
         _id=123,
         name="Newtest",
@@ -88,78 +97,63 @@ def test_edit_recipe(rec_driver):
         tags=["quick", "spicy"],
         notes=["notes"],
     )
-    assert result.name == "Newtest"
+    assert r._driver.recipe_edit.call_count == 1
 
 
-@pytest.mark.parametrize(
-    "tags, expected", [(["spicy"], 2), (["quick"], 1), (["slow"], 1)]
-)
-def test_find_recipes_by_tags(rec_driver, tags, expected):
+def test_find_recipes_by_tags():
     """
     GIVEN recipes in the DB with tags
     WHEN searching by given tag
-    THEN assert the correct # are returned
+    THEN assert the correct DB call is made
     """
-    r = RecipeUC(rec_driver)
-    result = r.find_recipes_by_tag(tags)
-    assert len(result) == expected
+    r = RecipeUC(Mock())
+    result = r.find_recipes_by_tag(["tag1", "tag2"])
+    assert r._driver.recipes_find_by_tag.call_count == 1
 
 
-def test_get_tags(rec_driver):
+def test_get_tags():
     """
     GIVEN a db with recipes
     WHEN requesting all discrete tags in all recipes
-    THEN assert the proper tags and number are returned
+    THEN assert the correct DB call is made
     """
-    r = RecipeUC(rec_driver)
+    r = RecipeUC(Mock())
     result = r.get_tags()
-    assert len(result) == 3
-    for tag in ["spicy", "slow", "quick"]:
-        assert tag in result
+    assert r._driver.recipes_get_tags.call_count == 1
 
 
-def test_delete_recipe_goodID(rec_driver):
+def test_delete_recipe():
     """
     GIVEN a db with recipes
     WHEN requesting to delete a specific recipe
-    THEN assert the recipe is marked as deleted
+    THEN assert 1 is returned
     """
-    r = RecipeUC(rec_driver)
+    r = RecipeUC(Mock())
+    r._driver.recipe_delete.return_value = 1
     result = r.delete_recipe(recipe_id=123)
     assert result == 1
 
 
-def test_delete_recipe_badID(rec_driver):
-    """
-    GIVEN a db with recipes
-    WHEN requesting to delete a nonexistent recipe
-    THEN assert 0 is returned
-    """
-    r = RecipeUC(rec_driver)
-    result = r.delete_recipe(recipe_id=789)
-    assert result == 0
-
-
-def test_recipes_search(rec_driver, mocker):
+def test_recipes_search():
     """
     GIVEN a db with recipes
     WHEN the user supplies a search string
-    THEN assert the recipes that match are returned
+    THEN assert the correct DB calls are made and combined upon return
     """
-    r = RecipeUC(rec_driver)
-    search_mock = mocker.patch.object(r, "recipes_search")
-    search_mock.return_value = ["r1", "r2"]
-    result = r.recipes_search("spam")
-    assert len(result)== 2
+    r = RecipeUC(Mock())
+    r._driver.recipes_find_by_name.return_value = ["test_recipe"]
+    r._driver.recipes_search.return_value = ["testing_recipe"]
+    result = r.recipes_search("test")
+    assert result == ["test_recipe", "testing_recipe"]
 
 
-def test_export_recipe_fileDoesNotExist(rec_driver, mocker):
+def test_export_recipe_fileDoesNotExist(mocker):
     """
     GIVEN a recipe in the DB
     WHEN the user requests to export the recipe for the first time
     THEN assert the correct functions are called
     """
-    r = RecipeUC(rec_driver)
+    r = RecipeUC(Mock())
     find_mock = mocker.patch.object(r, "find_recipe_by_id")
     date_mock = mocker.patch.object(datetime, "datetime")
     date_mock.strftime.return_value = "filename"
@@ -171,13 +165,13 @@ def test_export_recipe_fileDoesNotExist(rec_driver, mocker):
     assert export_mock.call_count == 1
 
 
-def test_export_recipe_fileAlreadyExists(rec_driver, mocker):
+def test_export_recipe_fileAlreadyExists(mocker):
     """
     GIVEN a recipe in the DB
     WHEN the user requests to export the recipe for a already existing file
     THEN assert the correct functions are called but no pdf is recreated
     """
-    r = RecipeUC(rec_driver)
+    r = RecipeUC(Mock())
     find_mock = mocker.patch.object(r, "find_recipe_by_id")
     date_mock = mocker.patch.object(datetime, "datetime")
     date_mock.strftime.return_value = "filename"
@@ -186,17 +180,17 @@ def test_export_recipe_fileAlreadyExists(rec_driver, mocker):
     export_mock = mocker.patch.object(export, "export_to_pdf")
     export_mock.return_value = "/path/to/file"
     result = r.export_recipe("12345")
-    assert find_mock.call_count == 1
+    assert r.find_recipe_by_id.call_count == 1
     assert export_mock.call_count == 0
 
 
-def test_import_recipe_from_url(mocker, rec_driver):
+def test_import_recipe_from_url(mocker):
     """
     GIVEN a recipe to import via url
     WHEN the url imported
     THEN assert it is sent to the database
     """
-    r = RecipeUC(rec_driver)
+    r = RecipeUC(Mock())
     import_mock = mocker.patch.object(ruc, "import_from_url")
     import_mock.return_value = {
         "images": "path_to_an_image"
@@ -211,13 +205,13 @@ def test_import_recipe_from_url(mocker, rec_driver):
     assert create_mock.call_count == 1
 
 
-def test_save_image(mocker, rec_driver):
+def test_save_image(mocker):
     """
     GIVEN a url-imported recipe with an image url in the data dict
     WHEN importing
     THEN assert the file is also downloaded and saved
     """
-    r = RecipeUC(rec_driver)
+    r = RecipeUC(Mock())
     req_mock = mocker.patch.object(requests, "get")
     req_mock.return_value.content.return_value = "imagedata"
     uuid_mock = mocker.patch.object(uuid, "uuid4")
